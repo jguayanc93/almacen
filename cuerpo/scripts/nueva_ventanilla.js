21b67b63bada8b4f7e3fd20209ec0f3b88705ae2
juan carlos <jguayancastillo21@gmail.com>
2026-04-14 16:39:45 -0500

    corregido los errores de consola al llamar a id que ya no existen

--- cuerpo/scripts/nueva_ventanilla.js
+++ cuerpo/scripts/nueva_ventanilla.js
@@ -90,45 +90,45 @@ socket.on('ventanilla mestro nuevos',(registros)=>{
     }
     document.getElementById("tablero-maestro-control-inicio").appendChild(cuerpo)
     /////////////////////ESTO PARA LA NUEVA VERSION AHORA DEBE CAMBIARSE POR LA NUEVA
-    let primeratablatitulo=document.getElementById("primeratablatitulo");
-    primeratablatitulo.className="text-center text-xl font-bold";
-    primeratablatitulo.textContent="NUEVOS DOCUMENTOS";
-    let primeratablamensaje=document.getElementById("primeratablamensaje");
-    primeratablamensaje.className="text-center text-lg font-medium";
-    primeratablamensaje.textContent="Documentos programados para su despacho en ventanilla";
-    let primeratabla=document.getElementById("tablero-maestro-control-inicio2");
-    primeratabla.innerHTML="";
-    for(let doc in registros){
-        const fecha=document.createElement('td')
-        fecha.className="border-b-2 truncate w-[14%]";
-        fecha.textContent=registros[doc][0];
+    // let primeratablatitulo=document.getElementById("primeratablatitulo");
+    // primeratablatitulo.className="text-center text-xl font-bold";
+    // primeratablatitulo.textContent="NUEVOS DOCUMENTOS";
+    // let primeratablamensaje=document.getElementById("primeratablamensaje");
+    // primeratablamensaje.className="text-center text-lg font-medium";
+    // primeratablamensaje.textContent="Documentos programados para su despacho en ventanilla";
+    // let primeratabla=document.getElementById("tablero-maestro-control-inicio2");
+    // primeratabla.innerHTML="";
+    // for(let doc in registros){
+    //     const fecha=document.createElement('td')
+    //     fecha.className="border-b-2 truncate w-[14%]";
+    //     fecha.textContent=registros[doc][0];
 
-        const hora=document.createElement('td')
-        hora.className="border-b-2 truncate w-[7%]";
-        hora.textContent=registros[doc][1];
+    //     const hora=document.createElement('td')
+    //     hora.className="border-b-2 truncate w-[7%]";
+    //     hora.textContent=registros[doc][1];
 
-        const documento=document.createElement('td')
-        documento.className="border-b-2 truncate w-[17%]";
-        documento.textContent=registros[doc][2];
+    //     const documento=document.createElement('td')
+    //     documento.className="border-b-2 truncate w-[17%]";
+    //     documento.textContent=registros[doc][2];
 
-        const cliente=document.createElement('td')
-        cliente.className="border-b-2 truncate w-[47%]";
-        cliente.textContent=registros[doc][3];
+    //     const cliente=document.createElement('td')
+    //     cliente.className="border-b-2 truncate w-[47%]";
+    //     cliente.textContent=registros[doc][3];
 
-        const zonas=document.createElement('td')
-        zonas.className="border-b-2 truncate w-[15%]";
-        zonas.textContent=registros[doc][4];
+    //     const zonas=document.createElement('td')
+    //     zonas.className="border-b-2 truncate w-[15%]";
+    //     zonas.textContent=registros[doc][4];
 
-        const fila=document.createElement('tr');
-        fila.className="hover:bg-black";
-        fila.appendChild(fecha)
-        fila.appendChild(hora)
-        fila.appendChild(documento)
-        fila.appendChild(cliente)
-        fila.appendChild(zonas)
+    //     const fila=document.createElement('tr');
+    //     fila.className="hover:bg-black";
+    //     fila.appendChild(fecha)
+    //     fila.appendChild(hora)
+    //     fila.appendChild(documento)
+    //     fila.appendChild(cliente)
+    //     fila.appendChild(zonas)
 
-        primeratabla.appendChild(fila);
-    }
+    //     primeratabla.appendChild(fila);
+    // }
 })
 
 socket.on('ventanilla mestro estados',(registros)=>{
@@ -182,29 +182,29 @@ socket.on('ventanilla mestro estados',(registros)=>{
     }
     document.getElementById("tablero-maestro-control-medio").appendChild(cuerpo)
     /////////////////////ESTO PARA LA NUEVA VERSION AHORA DEBE CAMBIARSE POR LA NUEVA
-    let segundatablatitulo=document.getElementById("segundatablatitulo");
-    segundatablatitulo.className="text-center text-xl font-bold";
-    segundatablatitulo.textContent="TABLERO CONTROL";
-    let segundatablamensaje=document.getElementById("segundatablamensaje");
-    segundatablamensaje.className="text-center text-lg font-medium";
-    segundatablamensaje.textContent="Estado del documento a trabajar";
-    let segundatabla=document.getElementById("tablero-maestro-control-medio2");
-    segundatabla.innerHTML="";
-    for(let doc in registros){
-        let grapador=document.createElement("div");
-        grapador.className="w-full flex flex-row justify-around";
+    // let segundatablatitulo=document.getElementById("segundatablatitulo");
+    // segundatablatitulo.className="text-center text-xl font-bold";
+    // segundatablatitulo.textContent="TABLERO CONTROL";
+    // let segundatablamensaje=document.getElementById("segundatablamensaje");
+    // segundatablamensaje.className="text-center text-lg font-medium";
+    // segundatablamensaje.textContent="Estado del documento a trabajar";
+    // let segundatabla=document.getElementById("tablero-maestro-control-medio2");
+    // segundatabla.innerHTML="";
+    // for(let doc in registros){
+    //     let grapador=document.createElement("div");
+    //     grapador.className="w-full flex flex-row justify-around";
 
-        const documento=document.createElement("div");
-        documento.className="w-[30%] font-bold";
-        documento.textContent=registros[doc][0];
+    //     const documento=document.createElement("div");
+    //     documento.className="w-[30%] font-bold";
+    //     documento.textContent=registros[doc][0];
 
-        const estado=document.createElement("div");
-        estado.className="w-[50%] bg-amber-500 rounded-md text-black font-mono font-bold";
-        estado.textContent=registros[doc][1];
-        grapador.appendChild(documento)
-        grapador.appendChild(estado);
-        segundatabla.appendChild(grapador);
-    }
+    //     const estado=document.createElement("div");
+    //     estado.className="w-[50%] bg-amber-500 rounded-md text-black font-mono font-bold";
+    //     estado.textContent=registros[doc][1];
+    //     grapador.appendChild(documento)
+    //     grapador.appendChild(estado);
+    //     segundatabla.appendChild(grapador);
+    // }
 })
 
 socket.on('ventanilla mestro terminados',(registros)=>{
@@ -281,43 +281,44 @@ socket.on('ventanilla mestro terminados',(registros)=>{
     }
     document.getElementById("tablero-maestro-control-fin").appendChild(cuerpo)
     /////////////////////ESTO PARA LA NUEVA VERSION AHORA DEBE CAMBIARSE POR LA NUEVA
-    let terceratablatitulo=document.getElementById("terceratablatitulo");
-    terceratablatitulo.className="text-center text-xl font-bold";
-    terceratablatitulo.textContent="TABLERO DESPACHO";
-    let terceratablamensaje=document.getElementById("terceratablamensaje");
-    terceratablamensaje.className="text-center text-lg font-medium";
-    terceratablamensaje.textContent="Checking del documento";
-    let terceratabla=document.getElementById("tablero-maestro-control-fin2");
-    terceratabla.innerHTML="";
-    for(let doc in registros){
-        let grapador=document.createElement("div");
-        grapador.className="w-full flex flex-row justify-center";
+    // let terceratablatitulo=document.getElementById("terceratablatitulo");
+    // terceratablatitulo.className="text-center text-xl font-bold";
+    // terceratablatitulo.textContent="TABLERO DESPACHO";
+    // let terceratablamensaje=document.getElementById("terceratablamensaje");
+    // terceratablamensaje.className="text-center text-lg font-medium";
+    // terceratablamensaje.textContent="Checking del documento";
+    // let terceratabla=document.getElementById("tablero-maestro-control-fin2");
+    // terceratabla.innerHTML="";
+    // for(let doc in registros){
+    //     let grapador=document.createElement("div");
+    //     grapador.className="w-full flex flex-row justify-center";
 
-        const documento=document.createElement("div");
-        documento.className="w-[30%] font-bold front-mono";
-        documento.textContent=registros[doc][0];
+    //     const documento=document.createElement("div");
+    //     documento.className="w-[30%] font-bold front-mono";
+    //     documento.textContent=registros[doc][0];
 
-        const estado=document.createElement('div');
-        estado.className="w-[5%]";
-        estado.textContent=registros[doc][1];
+    //     const estado=document.createElement('div');
+    //     estado.className="w-[5%]";
+    //     estado.textContent=registros[doc][1];
 
-        const boton=document.createElement("button");
-        if(registros[doc][1]==0){
-            boton.className="truncate w-[40%] bg-red-500 rounded-md text-white font-mono";
-            boton.textContent="FALTA";
-        }
-        else if(registros[doc][1]==1){
-            boton.setAttribute("id","chk"+registros[doc][0]);
-            boton.className="truncate w-[40%] bg-emerald-500 rounded-md text-white font-mono";
-            boton.textContent="CHECKING";
-            boton.addEventListener("click",()=>estado_cambio_checking(registros[doc][0],registros[doc][3],registros[doc][2],registros[doc][4]));
-        }
-        grapador.appendChild(documento)
-        grapador.appendChild(estado)
-        grapador.appendChild(boton)
-        terceratabla.appendChild(grapador);
+    //     const boton=document.createElement("button");
+    //     if(registros[doc][1]==0){
+    //         boton.className="truncate w-[40%] bg-red-500 rounded-md text-white font-mono";
+    //         boton.textContent="FALTA";
+    //     }
+    //     else if(registros[doc][1]==1){
+    //         boton.setAttribute("id","chk"+registros[doc][0]);
+    //         boton.className="truncate w-[40%] bg-emerald-500 rounded-md text-white font-mono";
+    //         boton.textContent="CHECKING";
+    //         boton.addEventListener("click",()=>estado_cambio_checking(registros[doc][0],registros[doc][3],registros[doc][2],registros[doc][4]));
+    //     }
+    //     grapador.appendChild(documento)
+    //     grapador.appendChild(estado)
+    //     grapador.appendChild(boton)
+    //     terceratabla.appendChild(grapador);
 
-    }
+    // }
+
     // for(let doc in registros){
     //     const documento=document.createElement('td')
     //     documento.className="truncate w-1/5 bg-red-500 rounded-md text-white font-mono";
